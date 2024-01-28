006985474e42783d7ba35337e0d42f558716385e
Natalia <ncalbuq@gmail.com>
2024-01-28 17:47:35 -0300

    update wallet

--- database/migrations/2024_01_27_151329_create_wallet_table.php
+++ database/migrations/2024_01_27_151329_create_wallet_table.php
@@ -14,7 +14,9 @@ return new class extends Migration
         Schema::create('wallets', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->double('balance');
+	    $table->double('balance');
+	    $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
